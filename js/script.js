179afad0a912091cbc10908413e34690cf8a8eb0
zellyo-digital0 <zellyo.digital@gmail.com>
2024-10-13 11:04:37 +0600

    v1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(index) {
+    const slides = document.querySelectorAll('.slider img');
+    if (index >= slides.length) slideIndex = 0;
+    if (index < 0) slideIndex = slides.length - 1;
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === slideIndex) ? 'block' : 'none';
+    });
+}
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+
+// top to bottom
+
+let topButton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
